4978e96d12eed47b7d35813d6962877a3bd73953
splaa <splaandrey@gmail.com>
2019-12-25 16:36:28 +0200

    + Added Validate UserJoinForm

--- models/user/UserJoinForm.php
+++ models/user/UserJoinForm.php
@@ -13,4 +13,16 @@
 		public $password;
 		public $password2;
 		
+		public function rules()
+		{
+			return [
+				[['name', 'email', 'password', 'password2'], 'required'],
+				['name', 'string', 'min' => 3, 'max' => 8],
+				['email', 'email'],
+				['password', 'string', 'min' => 4],
+				['password2', 'compare', 'compareAttribute' => 'password']
+			
+			];
+		}
+		
 	}
