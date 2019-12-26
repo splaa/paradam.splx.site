05d436d43956e31eb03ebc1601f8c5d8a908ea4f
serik_lav <serik_lav@mail.ru>
2019-12-26 15:27:20 +0200

    Migration finish

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 	return [
 		'class' => '\yii\db\Connection',
-		'dsn' => 'mysql:host=127.0.0.1;dbname=paradam',
-		'username' => 'splaa',
-		'password' => 'splaa1977'
+		'dsn' => 'mysql:host=localhost;dbname=paradam',
+		'username' => 'root',
+		'password' => 'root'
 	];
