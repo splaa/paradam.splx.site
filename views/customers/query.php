b264b14723b927d7b90c371232b82f3bc7fc310d
splaa <splaandrey@gmail.com>
2019-12-26 16:04:52 +0200

    + Added ignore git test/acceptance.suite.yml

--- views/customers/query.php
+++ views/customers/query.php
@@ -12,5 +12,6 @@
 <?= Html::beginForm(['/customers'], 'get') ?>
 <?= Html::label('Phone number to search:', 'phone_number') ?>
 <?= Html::textInput('phone_number') ?>
+<?= Html::textInput('number') ?>
 <?= Html::submitButton('Search') ?>
 <?= Html::endForm(); ?>
