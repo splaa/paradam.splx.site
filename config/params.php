79b9897320c8cf45bced686770d2ad9cda046d9c
serik_lav <serik_lav@mail.ru>
2019-12-31 12:09:26 +0200

    Add params to global params file

--- config/params.php
+++ config/params.php
@@ -13,8 +13,8 @@
 				'api_hash' => 'ff458dd210d0e2d278d802b87da8d3cb'
 			],
 			'smsc' => [
-				'login' => 'artisankava@gmail.com', // логин клиента
-				'password' => 'dasha2005', // пароль
+				'login' => 'yprabota@gmail.com', // логин клиента
+				'password' => '47Pdm,47', // пароль
 				'post' => '', // использовать метод POST
 				'https' => '', // использовать HTTPS протокол
 				'charset' => 'utf-8', // кодировка сообщения: utf-8, koi8-r или windows-1251 (по умолчанию)
