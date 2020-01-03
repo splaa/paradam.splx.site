694b4133fc96ec69c1b26c9792da2a7131ae57cb
splaa <splaandrey@gmail.com>
2020-01-03 15:04:24 +0200

    + Update Config Routes

--- config/common.php
+++ config/common.php
@@ -51,16 +51,13 @@
 				'rules' => [
 					'' => 'main/default/index',
 					'contact' => 'main/contact/index',
-					'<_a:(about|error)>' => 'main/default/<_a>',
-					'<_a:(login|logout)>' => 'user/default/<_a>',
+					'<_a:error>' => 'main/default/<_a>',
+					'<_a:(login|logout|signup|confirm-email|request-password-reset|reset-password)>' => 'user/default/<_a>',
 					
-					'<_m:[\w\-]+>' => '<_m>/default/index',
-					'<_m:[\w\-]+>/<id:\d+>' => '<_m>/default/view',
-					'<_m:[\w\-]+>/<id:\d+>/<_a:[\w-]+>' => '<_m>/default/<_a>',
-					'<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
 					'<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
 					'<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_m>/<_c>/<_a>',
-					'<_m:[\w\-]+>/<_c:[\w\-]+>/<_a:[\w-]+>' => '<_m>/<_c>/<_a>',
+					'<_m:[\w\-]+>' => '<_m>/default/index',
+					'<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
 				],
 			],
 			'mailer' => [
