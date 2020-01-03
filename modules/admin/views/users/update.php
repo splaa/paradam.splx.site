b3a0db85b986581f913b8ab5006bb911ca752018
splaa <splaandrey@gmail.com>
2020-01-03 16:20:23 +0200

    + Модуль администрирования и GridView

---
+++ modules/admin/views/users/update.php
@@ -0,0 +1,24 @@
+<?php
+	
+	use yii\helpers\Html;
+	
+	/* @var $this yii\web\View */
+	/* @var $model app\modules\user\models\User */
+	
+	$this->title = Yii::t('app', 'Update User: {name}', [
+		'name' => $model->id,
+	]);
+	$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'ADMIN'), 'url' => ['default/index']];
+	$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'ADMIN_USERS'), 'url' => ['index']];
+	$this->params['breadcrumbs'][] = ['label' => $model->username, 'url' => ['view', 'id' => $model->id]];
+	$this->params['breadcrumbs'][] = Yii::t('app', 'TITLE_UPDATE');
+?>
+<div class="user-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+	
+	<?= $this->render('_form', [
+		'model' => $model,
+	]) ?>
+
+</div>
