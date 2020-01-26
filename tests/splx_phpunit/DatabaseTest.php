b015f0abab5557801556f2cc21d7c6125515db0e
Андрей Лавринюк <splaandrey@gmail.com>
2020-01-26 20:45:26 +0200

    Update tests

---
+++ tests/splx_phpunit/DatabaseTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    protected static $dbh;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$dbh = new PDO('sqlite::memory:');
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$dbh = null;
+    }
+}
