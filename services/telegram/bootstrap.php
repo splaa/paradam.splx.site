6577761232b5da93a3cf5ca3ea11575e4fb4cf05
serik_lav <serik_lav@mail.ru>
2020-02-04 21:24:49 +0200

    add normal telegram service

--- services/telegram/bootstrap.php
+++ services/telegram/bootstrap.php
@@ -2,12 +2,12 @@
 //Check if autoload has been already loaded (in case plugin installed in existing project)
 $root = __DIR__;
 if (!file_exists($root . '/vendor/autoload.php')) {
-    $root = __DIR__ . '/../../..';
+    $root = __DIR__ . '/../../../';
 }
-require_once $root . '/vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 chdir($root);
 
 //Check if root env file hash been loaded (in case plugin installed in existing project)
 if (!getenv('SERVER_ADDRESS')) {
-    Dotenv\Dotenv::createImmutable($root)->load();
+    Dotenv\Dotenv::createImmutable(__DIR__)->load();
 }
