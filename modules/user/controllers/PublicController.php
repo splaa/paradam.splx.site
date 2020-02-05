f62ba7fee67e6ab2a192b527ada0448c422651d0
serik_lav <serik_lav@mail.ru>
2020-02-05 16:10:07 +0200

    Generate avatars finish

--- modules/user/controllers/PublicController.php
+++ modules/user/controllers/PublicController.php
@@ -21,16 +21,6 @@ class PublicController extends Controller
 		$model = User::findByUsername($username);
 
 		if ($model) {
-			$name = $model->username;
-			if (!empty($model->last_name)) {
-				$name = $model->first_name . ' ' . $model->last_name;
-			}
-			$avatar = new LetterAvatar($name, 'circle', User::SIZE_AVATAR_SMALL);
-			$avatar->saveAs('images/user/avatar/' . $model->username . '-' . User::SIZE_AVATAR_SMALL . '.png');
-			$avatar = new LetterAvatar($name, 'square',  User::SIZE_AVATAR_MEDIUM);
-			$avatar->saveAs('images/user/avatar/' . $model->username . '-' . User::SIZE_AVATAR_MEDIUM . '.png');
-			$avatar = new LetterAvatar($name, 'square', User::SIZE_AVATAR_BIG);
-			$avatar->saveAs('images/user/avatar/' . $model->username . '-' . User::SIZE_AVATAR_BIG . '.png');
 			$services = Service::find()->where('user_id' == $model->id)->all();
 
 			$messageForm = new MessageForm();
