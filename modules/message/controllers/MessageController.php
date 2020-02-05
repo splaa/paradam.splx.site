a1591b740c9159ab10f1e9f28af382df81849386
serik_lav <serik_lav@mail.ru>
2020-02-05 16:25:01 +0200

    Checker for balance change error text

--- modules/message/controllers/MessageController.php
+++ modules/message/controllers/MessageController.php
@@ -174,7 +174,7 @@ class MessageController extends UserController
 
 					return $this->redirect(Url::to(['view', 'id' => $hash->run(Hash::ENCODE)]));
 				} else {
-					Yii::$app->getSession()->setFlash('error', 'На счету не достаточно средств пожалуйста пополните Ваш аккаунт.');
+					Yii::$app->getSession()->setFlash('error', 'На счету не достаточно средств пожалуйста пополните Ваш баланс.');
 					return $this->redirect(Url::to(['/user/profile/balance']));
 				}
 			}
