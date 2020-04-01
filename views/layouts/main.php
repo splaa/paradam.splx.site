498eb3032f8092816aebf1df8b73c413a3cd823e
serik_lav <serik_lav@mail.ru>
2020-04-01 23:03:23 +0300

    add viewPort header disabled zoom after focus input

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,7 +15,7 @@ AppAsset::register($this);
 	<head>
 		<meta charset="<?= Yii::$app->charset ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
 		<?php $this->registerCsrfMetaTags() ?>
 
 		<title><?= Html::encode($this->title) ?></title>
